bf463ce32334e12f74d158adcc1f476beb8b8307
Igor Andrianov <andrianov2406@gmail.com>
2021-11-29 19:18:23 +0300

    Moved selectors to const.

--- cypress/integration/katanamrp/loginPage/login.js
+++ cypress/integration/katanamrp/loginPage/login.js
@@ -1,15 +1,18 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
 
+const PATH = '/login'
+const SELECTORS = {
+    getEmailInputField: () => cy.get('[name=email]'),
+    getPasswordInputField: () => cy.get('[name=password]'),
+}
+
 Given('I press "Sign in" button', () => {
-    cy.visit('/')
-    cy.contains('Got it!').click();
-    cy.get('[aria-label="Menu"]').click();
-    cy.contains('Sign in').click();
+    cy.visit(PATH)
 });
 
 Then(`I see "email" and "password" input fields`, () => {
-    cy.get('[name=email]')
+    SELECTORS.getEmailInputField()
         .should('be.visible');
-    cy.get('[name=password]')
+    SELECTORS.getPasswordInputField()
         .should('be.visible');
 });
--- cypress/integration/katanamrp/mainPage/main.js
+++ cypress/integration/katanamrp/mainPage/main.js
@@ -1,14 +1,21 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
 
+const PATH = '/'
+const SELECTORS = {
+    getTitleText: () => cy.contains('Everything you need to keep manufacturing'),
+    getCookiePolicyText: () => cy.contains('By visiting this website, you accept our cookie policy.'),
+    getCookiePolicyAcceptButton: () => cy.contains('Got it!'),
+}
+
 Given('I open the main page', () => {
-    cy.visit('/')
+    cy.visit(PATH)
 });
 
 Then(`I see "Katana" in the title and "accept our cookie policy" pop-up`, () => {
-    cy.title()
-        .should("have.value", 'Katana │ Manufacturing ERP Software for Total Visibility');
-    cy.contains('By visiting this website, you accept our cookie policy.')
+    SELECTORS.getTitleText()
+        .should('be.visible')
+    SELECTORS.getCookiePolicyText()
         .should('be.visible');
-    cy.contains('Got it!')
+    SELECTORS.getCookiePolicyAcceptButton()
         .should('be.visible');
 });
