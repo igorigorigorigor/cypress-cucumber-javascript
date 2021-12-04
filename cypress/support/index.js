1859c665e3b53474ba09ba79a1c3190b1102aa33
Igor Andrianov <i.andrianov@fun-box.ru>
2021-12-04 18:03:05 +0300

    Some refactoring.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -5,7 +5,15 @@ import LoginPage from "./pages/loginPage";
 import SalesOrdersPage from "./pages/salesOrdersPage";
 import AuthenticateAPI from "./apis/authentication/authenticateAPI";
 
-export const trialUser= new User(Cypress.env('credentials').trialUser.email,
+export const {
+    authenticateAPIRequestJsonSchema,
+} = require('./apis/authentication/authenticateAPIRequestJsonSchema');
+export const chai = require('chai');
+export const crypto = require("crypto");
+export const randomEmail = require('random-email');
+
+export const trialUser= new User(
+    Cypress.env('credentials').trialUser.email,
     Cypress.env('credentials').trialUser.password)
 
 export const mainPage= new MainPage();
@@ -13,14 +21,6 @@ export const loginPage= new LoginPage();
 export const salesOrdersPage= new SalesOrdersPage();
 
 export const authenticateAPI= new AuthenticateAPI();
-
-export const {
-    authenticateAPIRequestJsonSchema,
-} = require('./apis/authentication/authenticateAPIRequestJsonSchema');
-export const chai = require('chai');
-
-export const randomEmail = require('random-email');
-export const crypto = require("crypto");
 
 export default function getRandomString(length) {
     return crypto.randomBytes(length).toString('hex');
